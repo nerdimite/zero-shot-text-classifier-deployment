3532f33783bf521ab9032af08d97dda0e775f5ec
Bhavesh Laddagiri <bhavesh.laddagiri1@gmail.com>
2021-10-08 18:05:28 +0530

    add margin bottom to container

--- zero-shot-nlp-app/src/components/layout.js
+++ zero-shot-nlp-app/src/components/layout.js
@@ -1,5 +1,7 @@
 export const Container = (props) => {
   return (
-    <div className="md:container md:w-3/5 px-2 space-y-4">{props.children}</div>
+    <div className="md:container md:w-3/5 px-2 space-y-4 mb-10">
+      {props.children}
+    </div>
   );
 };
